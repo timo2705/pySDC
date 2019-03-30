bugfix for AC, somewhat faster, but still not there.. (factor 5)

pySDC/playgrounds/Dedalus/playground.py
@@ -1,12 +1,27 @@
 
 from dedalus import public as de
+from dedalus import core
 import numpy as np
 import matplotlib.pyplot as plt
+
+from pySDC.playgrounds.Dedalus.dedalus_field import dedalus_field
+
+# class wrapper(core.field.Field):
+#
+#     # def __init__(self, domain):
+#     #     super(wrapper, self).__init__(domain)
+#
+#     def __add__(self, other):
+#
+#         return wrapper(super(wrapper, self).__add__(other).evaluate(), self.domain)
+#     #
+#     def __abs__(self):
+#         return abs(self).evaluate()
 
 
 de.logging_setup.rootlogger.setLevel('INFO')
 
-xbasis = de.Fourier('x', 8, interval=(0,1), dealias=1)
+xbasis = de.Fourier('x', 128, interval=(0,1), dealias=1)
 
 
 domain = de.Domain([xbasis], grid_dtype=np.float64, comm=None)
@@ -22,7 +37,11 @@ print((f + g).evaluate())
 f['g'][:] = 1.0
 print(f['g'], g['g'])
 print(f, g)
-exit()
+
+
+
+
+# exit()
 g = domain.new_field()
 
 f2 = domain_2.new_field()
@@ -40,21 +59,21 @@ g['g'] = np.cos(2*np.pi*x)
 
 u = domain.new_field()
 
-xbasis_c = de.Fourier('x', 4, interval=(0,1), dealias=1)
-domain_c = de.Domain([xbasis_c],grid_dtype=np.float64, comm=None)
-
-fex = domain.new_field()
-fex['g'] = np.copy(f['g'])
-
-ff = domain.new_field()
-ff['g'] = np.copy(f['g'])
-ff.set_scales(scales=0.5)
-
-fc = domain_c.new_field()
-fc['g'] = ff['g']
-
-
-print(fc['g'].shape, fex['g'].shape)
+# xbasis_c = de.Fourier('x', 4, interval=(0,1), dealias=1)
+# domain_c = de.Domain([xbasis_c],grid_dtype=np.float64, comm=None)
+#
+# fex = domain.new_field()
+# fex['g'] = np.copy(f['g'])
+#
+# ff = domain.new_field()
+# ff['g'] = np.copy(f['g'])
+# ff.set_scales(scales=0.5)
+#
+# fc = domain_c.new_field()
+# fc['g'] = ff['g']
+#
+#
+# print(fc['g'].shape, fex['g'].shape)
 #
 # print((fc-fex).evaluate()['g'])
 
@@ -63,7 +82,13 @@ print(fc['g'].shape, fex['g'].shape)
 h = (f + g).evaluate()
 
 
-f_x = de.operators.differentiate(h, x=2).evaluate()
+hxx = de.operators.differentiate(h, x=2).evaluate()
+
+hxxex = domain.new_field()
+hxxex['g'] = -(2*np.pi)**2 * np.sin(2*np.pi*x) - (2*np.pi) ** 2 * np.cos(2*np.pi*x)
+
+print(max(abs(hxx - hxxex).evaluate()['g']))
+# exit()
 
 forcing = domain.new_field()
 forcing['g'] = -np.sin(np.pi * 2 * x) * (np.sin(0) - (np.pi * 2) ** 2 * np.cos(0))
@@ -122,23 +147,26 @@ problem.add_equation("dt(u) - dx(dx(u)) = RHS")
 ts = de.timesteppers.SBDF1
 solver = problem.build_solver(ts)
 u = solver.state['u']
-# u_old['g'] = np.copy(f['g'])
+# u['g'] = np.sin(2*np.pi*x)
+tmp = np.tanh((0.25 - np.sqrt((x -0.5) ** 2)) / (np.sqrt(2) * 0.04))
+u['g'] = tmp
 
+dt = 1.912834231231E+07
 t = 0.0
 for n in range(nsteps):
-    u['g'] = u['g'] - dt * np.sin(np.pi * 2 * x) * (np.sin(t) - (np.pi * 2) ** 2 * np.cos(t))
-    # problem.parameters['RHS'] = u_old + forcing
+    # u['g'] = u['g'] - dt * np.sin(np.pi * 2 * x) * (np.sin(t) - (np.pi * 2) ** 2 * np.cos(t))
     solver.step(dt)
     t += dt
-    # forcing['g'] = -np.sin(np.pi * 2 * x) * (np.sin(t) - (np.pi * 2) ** 2 * np.cos(t))
-    # u_old['g'] = 1.0 / dt * u['g']
-    # u['g'] -= de.operators.differentiate(u_old, x=2)*dt
+
+    uxx = de.operators.differentiate(u, x=2).evaluate()
+    print(max(abs(u['g'] - dt*uxx['g'] - tmp)))
+    exit()
 
 print(np.linalg.norm(u['g']-uex['g'], np.inf))
 
-#
-plt.figure(1)
-plt.plot(x,u['g'])
-plt.plot(x,uex['g'])
-#
-plt.pause(1)
+# #
+# plt.figure(1)
+# plt.plot(x,u['g'])
+# plt.plot(x,uex['g'])
+# #
+# plt.pause(1)

pySDC/playgrounds/Dedalus/playground_parallel.py
@@ -19,8 +19,8 @@ space_rank = space_comm.Get_rank()
 
 de.logging_setup.rootlogger.setLevel('INFO')
 
-xbasis = de.Fourier('x', 4, interval=(0, 1), dealias=1)
-ybasis = de.Fourier('y', 4, interval=(0, 1), dealias=1)
+xbasis = de.Fourier('x', 128, interval=(0, 1), dealias=1)
+ybasis = de.Fourier('y', 128, interval=(0, 1), dealias=1)
 
 domain = de.Domain([xbasis, ybasis], grid_dtype=np.float64, comm=space_comm)
 
@@ -86,13 +86,21 @@ problem.add_equation("dt(u) - dx(dx(u)) - dy(dy(u)) = 0")
 ts = de.timesteppers.SBDF1
 solver = problem.build_solver(ts)
 u = solver.state['u']
-
+# tmp = np.tanh((0.25 - np.sqrt((x - 0.5) ** 2 + (y - 0.5) ** 2)) / (np.sqrt(2) * 0.04))
+# tmp =
+# print(tmp)
+u['g'] = np.tanh((0.25 - np.sqrt((x - 0.5) ** 2 + (y - 0.5) ** 2)) / (np.sqrt(2) * 0.04))
+u_old = domain.new_field()
+u_old['g'] = u['g']
 
 t = 0.0
 for n in range(nsteps):
-    u['g'] = u['g'] - dt * np.sin(np.pi * 2 * x) * np.sin(2*np.pi*y) * (np.sin(t) - 2.0 * (np.pi * 2) ** 2 * np.cos(t))
+    # u['g'] = u['g'] - dt * np.sin(np.pi * 2 * x) * np.sin(2*np.pi*y) * (np.sin(t) - 2.0 * (np.pi * 2) ** 2 * np.cos(t))
     solver.step(dt)
     t += dt
+    uxx = (de.operators.differentiate(u, x=2) + de.operators.differentiate(u, y=2)).evaluate()
+    print(np.amax(abs(u['g'] - dt * uxx['g'] - u_old['g'])))
+    exit()
 # print(t, nsteps)
 
 local_norm = np.amax(abs(u['g']-uex['g']))