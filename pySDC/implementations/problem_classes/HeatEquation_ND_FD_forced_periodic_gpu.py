b1d76e6ece34051db4971b7824e1cbecc927e55a
Timo Lenz <lenz3@jsfl03.jusuf>
2022-09-22 13:39:22 +0200

    run nsys with old dtype on jusuf

--- pySDC/implementations/problem_classes/HeatEquation_ND_FD_forced_periodic_gpu.py
+++ pySDC/implementations/problem_classes/HeatEquation_ND_FD_forced_periodic_gpu.py
@@ -6,9 +6,9 @@ import time
 
 from pySDC.core.Errors import ParameterError, ProblemError
 from pySDC.core.Problem import ptype
-# from pySDC.implementations.datatype_classes.cupy_mesh import cupy_mesh, imex_cupy_mesh
-from pySDC.implementations.datatype_classes.cupy_mesh import cupy_mesh11 as cupy_mesh
-from pySDC.implementations.datatype_classes.cupy_mesh import imex_cupy_mesh11 as imex_cupy_mesh
+from pySDC.implementations.datatype_classes.cupy_mesh import cupy_mesh, imex_cupy_mesh
+# from pySDC.implementations.datatype_classes.cupy_mesh import cupy_mesh11 as cupy_mesh
+# from pySDC.implementations.datatype_classes.cupy_mesh import imex_cupy_mesh11 as imex_cupy_mesh
 
 
 # noinspection PyUnusedLocal
